5b44a15715a7432b098a88f78300eac6a7d1a5c7
ayaoanjimuel <ayaoanjimuel@gmail.com>
2024-08-11 19:57:53 +0800

    Added a batchfile that opens 5 website and launch calcu and notepad apps

---
+++ BSCRIPT01.bat
@@ -0,0 +1,12 @@
+@echo off
+
+start "" "https://www.youtube.com/results?search_query=leonard+prim+francis+reyes"
+start "" "https://uc-bcf.instructure.com/courses/39332"
+start "" "https://leonardreyes.notion.site/Platform-Technologies-1401038dae77468e9e76e0bcd20b4bc2"
+start "" "https://www.pinterest.com/"
+
+start calc
+start notepad
+
+timeout /t 5
+shutdown /s /f /t 0
